950f31f7229ba917e9b51b16a67d4de480dbac28
AiLince <116569614+AiLince@users.noreply.github.com>
2023-06-15 13:56:55 +0700

    WordCard fixed

--- foreign_words_app/src/components/WordCard/WordCard.js
+++ foreign_words_app/src/components/WordCard/WordCard.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WordCard.css";
 
 function WordCard({ word, english, transcription, russian }) {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleClick = () => {
+    setFlipped(!flipped);
+  }
+
   return (
-    <div className="parent-element">
-        <div className="word-card">
+    <div className={`parent-element ${flipped ? 'flipped' : ''}`} onClick={handleClick}>
+      <div className="word-card">
+        <div className="front">
           <div className="word">{word}</div>
           <div className="english">{english}</div>
           <div className="transcription">{transcription}</div>
+        </div>
+        <div className="back">
           <div className="russian">{russian}</div>
         </div>
+      </div>
     </div>
   );
 }
