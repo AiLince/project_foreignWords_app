f88b32c68366e7e7542c26679c1e086e9a5eeaf4
AiLince <116569614+AiLince@users.noreply.github.com>
2023-06-29 10:34:18 +0700

    25 week's updates

--- foreign_words_app/src/components/WordCarousel/WordCarousel.js
+++ foreign_words_app/src/components/WordCarousel/WordCarousel.js
@@ -2,87 +2,88 @@ import React, { useState, useEffect, useCallback } from "react";
 import WordCard from "../WordCard/WordCard";
 import "./WordCarousel.css";
 
-function WordCarousel({ words, disableFirstAndLast }) {
-    const [currentIndex, setCurrentIndex] = useState(0);
+function WordCarousel({ words, disableFirstAndLast, defaultIndex }) {
+    const [currentIndex, setCurrentIndex] = useState(defaultIndex || 0);
     const [flipped, setFlipped] = useState(false);
 
     const handlePrevClick = useCallback(() => {
-    if (currentIndex > 0) {
-        setCurrentIndex(currentIndex - 1);
-    } else {
-        if (disableFirstAndLast) {
-        setCurrentIndex(words.length - 1);
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        } else {
+            if (disableFirstAndLast) {
+                setCurrentIndex(words.length - 1);
+            }
         }
-    }
-    setFlipped(false);
+        setFlipped(false);
     }, [currentIndex, disableFirstAndLast, words]);
 
     const handleNextClick = useCallback(() => {
-    if (currentIndex < words.length - 1) {
-        setCurrentIndex(currentIndex + 1);
-    } else {
-        if (disableFirstAndLast) {
-        setCurrentIndex(0);
+        if (currentIndex < words.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        } else {
+            if (disableFirstAndLast) {
+                setCurrentIndex(0);
+            }
         }
-    }
-    setFlipped(false);
+        setFlipped(false);
     }, [currentIndex, disableFirstAndLast, words]);
 
     useEffect(() => {
-    const handleKeyDown = (event) => {
-        if (event.key === "ArrowLeft") {
-        handlePrevClick();
-        } else if (event.key === "ArrowRight") {
-        handleNextClick();
-        }
-    };
+        const handleKeyDown = (event) => {
+            if (event.key === "ArrowLeft") {
+                handlePrevClick();
+            } else if (event.key === "ArrowRight") {
+                handleNextClick();
+            }
+        };
 
-    document.addEventListener("keydown", handleKeyDown);
+        document.addEventListener("keydown", handleKeyDown);
 
-    return () => {
-        document.removeEventListener("keydown", handleKeyDown);
-    };
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
     }, [handlePrevClick, handleNextClick]);
 
     return (
-    <div className="word-carousel">
-        {words[currentIndex] ? (
-        <WordCard
-            key={currentIndex}
-            word={words[currentIndex].word}
-            english={words[currentIndex].english}
-            transcription={words[currentIndex].transcription}
-            russian={words[currentIndex].russian}
-            flipped={flipped}
-            setFlipped={setFlipped}
-        />
-        ) : (
-        <p>Идёт загрузка. Пожалуйста, подождите</p>
-        )}
-        <div className="buttons">
-        <button
-            className="prev"
-            title="Предыдущее слово"
-            onClick={handlePrevClick}
-            disabled={!disableFirstAndLast && currentIndex === 0}
-        >
-            &#8592;
-        </button>
-        <button
-            className="next"
-            title="Следующее слово"
-            onClick={handleNextClick}
-            disabled={!disableFirstAndLast && currentIndex === words.length - 1}
-        >
-            &#8594;
-        </button>
+        <div className="word-carousel">
+            {words[currentIndex] ? (
+                <WordCard
+                    key={currentIndex}
+                    word={words[currentIndex].word}
+                    english={words[currentIndex].english}
+                    transcription={words[currentIndex].transcription}
+                    russian={words[currentIndex].russian}
+                    flipped={flipped}
+                    setFlipped={setFlipped}
+                />
+            ) : (
+                <p>Идёт загрузка. Пожалуйста, подождите</p>
+            )}
+            <div className="buttons">
+                <button
+                    className="prev"
+                    title="Предыдущее слово"
+                    onClick={handlePrevClick}
+                    disabled={!disableFirstAndLast && currentIndex === 0}
+                >
+                    &#8592;
+                </button>
+                <div className="current-slide">
+                    {currentIndex + 1}/{words.length}
+                </div>
+                <button
+                    className="next"
+                    title="Следующее слово"
+                    onClick={handleNextClick}
+                    disabled={!disableFirstAndLast && currentIndex === words.length - 1}
+                >
+                    &#8594;
+                </button>
+            </div>
         </div>
-    </div>
     );
 }
 
-WordCarousel.defaultProps = {
-    words: []
-};
+WordCarousel.defaultProps = { words: [], defaultIndex: 0 };
 
 export default WordCarousel;
